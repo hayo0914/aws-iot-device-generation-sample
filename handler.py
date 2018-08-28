489f71cd6f471945770ffa6efcfacbf711a13838
hayo0914 <hayo0914@gmail.com>
2018-08-28 15:57:37 +0900

    progress

--- handler.py
+++ handler.py
@@ -2,29 +2,25 @@ import json
 import device_manager
 
 def generateCert(event, context):
+    """
+    Assume event contains thingName of the thing which will be created
+    """
     deviceManager = device_manager.DeviceManager()
-    deviceManager.createThing('test123456')
+    deviceManager.createThing(event["thingName"])
     deviceManager.createKeysAndCertificate()
     deviceManager.attachThingPrincipal()
     deviceManager.attachPolicy()
-
     body = {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "input": event
+        "certificateArn" : deviceManager.certificateArn,
+        "certificateId"  : deviceManager.certificateId,
+        "certificatePem" : deviceManager.certificatePem,
+        "keyPair"        : deviceManager.keyPair,
+        "thingName"      : deviceManager.thingName,
+        "thingArn"       : deviceManager.thingArn,
+        "thingId"        : deviceManager.thingId,
     }
-
     response = {
         "statusCode": 200,
         "body": json.dumps(body)
     }
-
     return response
-
-    # Use this code if you don't use the http event with the LAMBDA-PROXY
-    # integration
-    """
-    return {
-        "message": "Go Serverless v1.0! Your function executed successfully!",
-        "event": event
-    }
-    """
